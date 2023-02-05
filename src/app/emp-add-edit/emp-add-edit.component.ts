2e38651fd050c10caecec33e30b82e74038b42b6
Siegfried Boeken <37549326+VarVergErgos@users.noreply.github.com>
2023-02-05 11:00:42 +0100

    first commit

---
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -0,0 +1,101 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CoreService } from '../core/core.service';
+import { EmployeeService } from '../services/employee.service';
+
+
+@Component({
+  selector: 'app-emp-add-edit',
+  templateUrl: './emp-add-edit.component.html',
+  styleUrls: ['./emp-add-edit.component.scss'],
+})
+export class EmpAddEditComponent implements OnInit {
+  empForm: FormGroup;
+
+  education: string[] = [
+    'Matric',
+    'Diploma',
+    'Intermediate',
+    'Graduate',
+    'Post Graduate',
+  ];
+
+  constructor(
+    private _fb: FormBuilder,
+    private _empService: EmployeeService,
+    private _dialogRef: MatDialogRef<EmpAddEditComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private _coreService: CoreService
+  ) {
+    this.empForm = this._fb.group({
+      firstName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(25),
+      ]),
+      lastName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(25),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      dob: new FormControl('', [Validators.required]),
+      gender: new FormControl('', [Validators.required]),
+      education: new FormControl('', [Validators.required]),
+      company: new FormControl('', [Validators.required]),
+      experience: new FormControl(null, [
+        Validators.required,
+        Validators.min(0),
+        Validators.max(200),
+        Validators.pattern(/^[0-9]+(\.[0-9]{1,2})?$/),
+      ]),
+      package: new FormControl(null, [
+        Validators.required,
+        Validators.min(0),
+        Validators.max(200),
+        Validators.pattern(/^[0-9]+(\.[0-9]{1,2})?$/),
+      ]),
+    });
+  }
+
+  ngOnInit(): void {
+    this.empForm.patchValue(this.data);
+  }
+
+
+
+  public checkError = (controlName: string, errorName: string) => {
+    console.log(this.empForm.value);
+    console.log(this.empForm.valid);
+    return this.empForm.controls[controlName].hasError(errorName);
+  };
+
+  onFormSubmit() {
+    if (this.empForm.valid) {
+      if (this.data) {
+        this._empService
+          .updateEmployee(this.data.id, this.empForm.value)
+          .subscribe({
+            next: (val: any) => {
+              this._coreService.openSnackBar('Employee detail updated!');
+              this._dialogRef.close(true);
+            },
+            error: (err: any) => {
+              console.error(err);
+            },
+          });
+      } else {
+        this._empService.addEmployee(this.empForm.value).subscribe({
+          next: (val: any) => {
+            this._coreService.openSnackBar('Employee added successfully');
+            this._dialogRef.close(true);
+          },
+          error: (err: any) => {
+            console.error(err);
+          },
+        });
+      }
+    }
+  }
+}
